473d248bf04a00110194cd5d33935f3cd056553f
Gabriel Rapucci <gabrielrapuccigonzalez14@hotmail.com>
2023-07-06 18:58:03 -0300

    Add, configure and generate next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || 'https://personal-website-gabrielrg.vercel.app/',
+  generateRobotsTxt: true,
+}
