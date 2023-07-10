0ed0b2b4fb65603fb43e8a714732aa62c4b5c794
Gabriel Rapucci <gabrielrapuccigonzalez14@hotmail.com>
2023-07-10 11:58:21 -0300

    Configure next.js X-Robots-Tag header to index robots

--- next.config.js
+++ next.config.js
@@ -18,4 +18,21 @@ const nextConfig = withPWA({
   }
 })
 
-module.exports = nextConfig
+module.exports = {
+  ...nextConfig,
+  async headers() {
+    const headers = [];
+    if (process.env.NEXT_PUBLIC_VERCEL_ENV === "production") {
+      headers.push({
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'index, follow',
+          },
+        ],
+        source: '/:path*',
+      });
+    }
+    return headers;
+  },
+}
