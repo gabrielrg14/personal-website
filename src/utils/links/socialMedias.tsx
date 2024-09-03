8c0bbdfa2aa73bbc560d7a39097d3d96662d1a0c
Gabriel Rapucci <gabrielrapuccigonzalez14@hotmail.com>
2024-09-02 23:36:08 -0300

    Add WhatsApp link to footer

--- src/utils/links/socialMedias.tsx
+++ src/utils/links/socialMedias.tsx
@@ -2,6 +2,7 @@ import { USERNAME } from "common"
 import { MarkGithub } from "@styled-icons/octicons/MarkGithub"
 import { Linkedin } from "@styled-icons/simple-icons/Linkedin"
 import { Instagram } from "@styled-icons/simple-icons/Instagram"
+import { Whatsapp } from "@styled-icons/simple-icons/Whatsapp"
 
 export const socialMedias = [
     {
@@ -18,5 +19,10 @@ export const socialMedias = [
         name: "Instagram",
         link: "https://www.instagram.com/gabriel_rapucci/",
         icon: <Instagram />
+    },
+    {
+        name: "WhatsApp",
+        link: "https://api.whatsapp.com/send/?phone=5519981392516&text&type=phone_number&app_absent=0/",
+        icon: <Whatsapp />
     }
 ]
