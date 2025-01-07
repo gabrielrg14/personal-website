22a766f9bffab0100cf9cd4a00fd69fb340c4c45
Gabriel Rapucci <gabrielrapuccigonzalez14@hotmail.com>
2025-01-07 20:33:07 -0300

    Add e2e navigation tests

--- cypress/e2e/navigation.cy.ts
+++ cypress/e2e/navigation.cy.ts
@@ -82,10 +82,24 @@ describe("Navigation", () => {
         )
     })
 
+    it("validate the E-mail link in the footer", () => {
+        cy.validateFooterLink(
+            "E-mail",
+            "mailto:gabrielrapuccigonzalez14@hotmail.com"
+        )
+    })
+
     it("validate the Instagram link in the footer", () => {
         cy.validateFooterLink(
             "Instagram",
             "https://www.instagram.com/gabriel_rapucci/"
+        )
+    })
+
+    it("validate the WhatsApp link in the footer", () => {
+        cy.validateFooterLink(
+            "WhatsApp",
+            "https://api.whatsapp.com/send/?phone=5519981392516&text&type=phone_number&app_absent=0/"
         )
     })
 })
